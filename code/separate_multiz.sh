f793aa4950c855b20ecbbc56a124da91a57a096c
Maria <99797026+tiamariabailey@users.noreply.github.com>
2022-10-24 22:36:39 +0200

    Update separate_multiz.sh

--- code/separate_multiz.sh
+++ code/separate_multiz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Separating entries (delimiter = 2 new lines):
-awk -v RS='\n\n\n' '{print > ("enst-" NR ".txt")}' multiztest.txt
+awk -v RS='\n\n\n' '{print > ("enst-" NR ".txt")}' knownCanonical.multiz100way.exonAA.fa
 
 #Renaming files:
 for i in enst-*;
@@ -9,6 +9,6 @@ do
  ensemblid=$(grep -o ENST[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] "$i" | sort -u);
  new_filename="${ensemblid}.txt";
  if [ ! -f "$new_filename" ]; then
-    echo mv "$i" "$new_filename";
+    mv "$i" "$new_filename";
  fi;
 done
