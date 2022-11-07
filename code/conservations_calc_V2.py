31d0a08ce736d8198c96f405ecac85b815bd364e
Dries Oome <57561283+DriesOome@users.noreply.github.com>
2022-11-07 18:47:50 +0100

    Add files via upload

--- code/conservations_calc_V2.py
+++ code/conservations_calc_V2.py
@@ -4,10 +4,13 @@ Created on Mon Nov  7 10:36:47 2022
 
 @author: janve
 """
+from joblib import Parallel, delayed
+
 
 def isGlycolationSite(seq,index):
-    try: 
-        if list(seq)[index+1] == 'N' and list(seq)[index+2] != 'P' and (list(seq)[index+3] == 'S' or list(seq)[index+3] == 'T'):
+    #print(seq[index-3:index+3])
+    try:
+        if list(seq)[index] == 'N' and list(seq)[index+1] != 'P' and (list(seq)[index+2] == 'S' or list(seq)[index+2] == 'T'):
             return True
         else:
             return False
@@ -16,23 +19,37 @@ def isGlycolationSite(seq,index):
     
 
 import pandas as pd
-data1 = pd.read_csv('dpPTMextended.tsv',sep = "\t")
+dbPTM = pd.read_csv('../data/dpPTMextended.tsv', sep ="\t")
 
-data2 = pd.read_csv("compiledMultizDatabase.csv", names=["id", "seq"])
-data2["organism"] = [item.split(".")[1].split("_")[1] for item in data2["id"]]
-data2["id"] = [item.split(".")[0] for item in data2["id"]]
-data1['conservation'] = 0
+multiz = pd.read_csv("../data/compiledMultizDatabase.csv", names=["id", "seq"])
+multiz["organism"] = [item.split(".")[1].split("_")[1] for item in multiz["id"]]
+multiz["id"] = [item.split(".")[0] for item in multiz["id"]]
+dbPTM['conservation'] = 0
 
-for i in range(0,len(data1["ensId"])):
-    for j in range(0, len(data2['id'])):
-        if data1['ensId'][i] == data2['id'][j] and data2['organism'][j] == 'hg38':
-            print('next prot')
-            index = data1['index'][i]
-            seq = data2['seq'][j]
-            print(list(seq)[index+1])
-            if isGlycolationSite(seq, index) == True:
-                data1['conservation'][i] += 1
-        if data1['ensId'][i] == data2['id'][j]:
-            seq = data2['seq'][j]
-            if isGlycolationSite(seq, index):
-                data1['conservation'][i] += 1
+def calculateConservationForProtein(rowIndex, glycoIndex, subMultiz):
+    conservationCounter = 0
+    # print(subMultiz.loc[subMultiz["organism"]=="hg38"]["seq"].item()[glycoIndex-10:glycoIndex+10])
+    if isGlycolationSite(subMultiz.loc[subMultiz["organism"]=="hg38"]["seq"].item(), glycoIndex-1) == True:
+        conservationCounter += 1
+        subMultiz = subMultiz.drop(subMultiz[subMultiz["organism"]=="hg38"].index, axis=0)
+        # print(list(humanSequence)[index-10:index+10])
+        for subMultizIndex, subMultizRow in subMultiz.iterrows():
+            if isGlycolationSite(subMultizRow["seq"], glycoIndex-1) == True:
+                conservationCounter += 1
+    print(conservationCounter)
+    return rowIndex, conservationCounter
+
+
+results = Parallel(n_jobs=60, verbose=10)(
+        delayed(calculateConservationForProtein)(rowIndex, row["index"], multiz[multiz["id"] == row["ensId"]].reset_index(drop=True))
+        for rowIndex, row in dbPTM.iterrows()
+)
+
+for result in results:
+    try:
+        dbPTM.loc[result[0], "conservation"] = result[1]
+    except:
+        print("failed")
+dbPTM.to_csv("../data/dpPTMextended.tsv", sep="\t", index=False)
+
+
